ca2f6282f8786e5fa03b9e7f039f15d18fbb1878
tlzxwwt <55043330+tlzxwwt@users.noreply.github.com>
2020-03-02 20:38:39 +0800

    Add files via upload

--- data_structure.h
+++ data_structure.h
@@ -15,6 +15,7 @@ void Help(ostream &of)
 	of<<endl;	
 	of<<"目前支持的数据结构："<<endl;
 	of<<"UFSET\t并查集"<<endl;
+	of<<"SEGTREEA\t线段树A"<<endl;
 	of<<"SEGTREEB\t线段树B"<<endl;
 }
 inline void ReadNum(int &s)
@@ -54,27 +55,27 @@ inline void ReadNum(long long &s)
 class UFSET
 {
 	public :
-		NUM size;
-		vector<NUM> father;
-		UFSET(NUM a=10010)
+		int size;
+		vector<int> father;
+		UFSET(int a=10010)
 		{
 			size=a;
-			for(NUM i=1;i<=size;i++)
+			for(int i=1;i<=size;i++)
 			{
 				father.push_back(i);
 			}
 		}
-		NUM FindFather(NUM k)
+		int FindFather(int k)
 		{
 			if(father[k]==k) return k;
 			father[k]=FindFather(father[k]);
 			return father[k];
 		}
-		void Union(NUM k1,NUM k2)
+		void Union(int k1,int k2)
 		{
 			father[FindFather(k1)]=FindFather(k2);
 		}
-		bool IfSameSet(NUM k1,NUM k2)
+		bool IfSameSet(int k1,int k2)
 		{
 			return FindFather(k1)==FindFather(k2);
 		}
@@ -82,10 +83,10 @@ class UFSET
 class SEGTREENODE
 {
 	public:
-		NUM l,r,m;
+		int l,r,m;
 		NUM v;
 		NUM lazy;
-		SEGTREENODE(NUM a=1,NUM b=1,NUM c=0)
+		SEGTREENODE(int a=1,int b=1,NUM c=0)
 		{
 			l=a;
 			r=b;
@@ -94,12 +95,49 @@ class SEGTREENODE
 			lazy=0;
 		}
 };
+class SEGTREEA
+{
+	public:
+		int size;
+		NUM (*f)(NUM,NUM);
+		vector<SEGTREENODE> tree;
+		void BuildTree(int pos,int l,int r,NUM a[])
+		{
+			tree[pos]=SEGTREENODE(l,r,a[l]);
+			if(l==r) return;
+			BuildTree(pos*2,l,tree[pos].m,a);
+			BuildTree(pos*2+1,tree[pos].m+1,r,a);
+			tree[pos].v=f(tree[pos*2].v,tree[pos*2+1].v);
+		}
+		SEGTREEA(int s,NUM (*p)(NUM,NUM),NUM a[])
+		{
+			size=s;
+			f=p;
+			tree.resize(s*4+1);
+			BuildTree(1,1,size,a);
+		}
+		void Change(int pos,const int &k,const NUM &v)
+		{
+			if(tree[pos].l==k&&tree[pos].r==k)
+			{
+				tree[pos].v=v;
+				return;
+			}
+			if(tree[pos].m<k) Change(pos*2+1,k,v);
+			else Change(pos*2,k,v);
+			tree[pos].v=f(tree[pos*2].v,tree[pos*2+1].v);
+		}
+		void Change(const int &k,const NUM &v)
+		{
+			Change(1,k,v);
+		}				
+};
 class SEGTREEB
 {
 	public:
 		NUM size;
 		vector<SEGTREENODE> tree;
-		void BuildTree(NUM pos,NUM l,NUM r,NUM a[])
+		void BuildTree(int pos,int l,int r,NUM a[])
 		{
 			tree[pos]=SEGTREENODE(l,r,a[l]);
 			if(l==r) return;
@@ -107,13 +145,13 @@ class SEGTREEB
 			BuildTree(pos*2+1,tree[pos].m+1,r,a);
 			tree[pos].v=tree[pos*2].v+tree[pos*2+1].v;
 		}
-		SEGTREEB(NUM s,NUM a[])
+		SEGTREEB(int s,NUM a[])
 		{
 			size=s;
 			tree.resize(s*4+1);
 			BuildTree(1,1,size,a);
 		}
-		void Down(const NUM &pos)
+		void Down(const int &pos)
 		{
 			tree[pos*2].lazy+=tree[pos].lazy;
 			tree[pos*2+1].lazy+=tree[pos].lazy;
@@ -121,7 +159,23 @@ class SEGTREEB
 			tree[pos*2+1].v+=tree[pos].lazy*(tree[pos*2+1].r-tree[pos*2+1].l+1);
 			tree[pos].lazy=0;
 		}
-		void AddRange(NUM pos,const NUM &l,const NUM &r,const NUM &v)
+		void Change(int pos,const int &k,const NUM &v)
+		{
+			if(tree[pos].l==k&&tree[pos].r==k)
+			{
+				tree[pos].v=v;
+				return;
+			}
+			if(tree[pos].lazy!=0) Down(pos);
+			if(tree[pos].m<k) Change(pos*2+1,k,v);
+			else Change(pos*2,k,v);
+			tree[pos].v=tree[pos*2].v+tree[pos*2+1].v;
+		}
+		void Change(const int &k,const NUM &v)
+		{
+			Change(1,k,v);
+		}		
+		void AddRange(int pos,const int &l,const int &r,const NUM &v)
 		{
 			if(tree[pos].l>=l&&tree[pos].r<=r)
 			{
@@ -134,11 +188,11 @@ class SEGTREEB
 			if(tree[pos].m<r) AddRange(pos*2+1,l,r,v);
 			tree[pos].v=tree[pos*2].v+tree[pos*2+1].v;
 		}
-		void AddRange(const NUM &l,const NUM &r,const NUM &v)
+		void AddRange(const int &l,const int &r,const NUM &v)
 		{
 			AddRange(1,l,r,v);
 		}
-		NUM Sum(NUM pos,const NUM &l,const NUM &r)
+		NUM Sum(int pos,const int &l,const int &r)
 		{
 			if(tree[pos].l>=l&&tree[pos].r<=r)
 			{
@@ -150,7 +204,7 @@ class SEGTREEB
 			if(tree[pos].m<r) ts+=Sum(pos*2+1,l,r);
 			return ts;
 		}
-		NUM sum(const NUM &l,const NUM &r)
+		NUM sum(const int &l,const int &r)
 		{
 			return Sum(1,l,r);
 		}
